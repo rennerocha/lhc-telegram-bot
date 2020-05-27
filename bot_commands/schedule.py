4a161cc4e53b09171c7615f3c514c770b643810b
Renne Rocha <renne@rennerocha.com>
2020-05-27 17:12:47 -0300

    Unpin pinned messages of past events

--- bot_commands/schedule.py
+++ bot_commands/schedule.py
@@ -3,31 +3,50 @@ import logging
 
 from dynaconf import settings
 from ics import Calendar
+
+from models import PinnedMessages
 from utils.ics_calendar import lhc_ics
 
 logging.basicConfig(level=settings.LOG_LEVEL)
 logger = logging.getLogger("joker")
 
 
-def quando(update, context):
+def get_events(when=""):
     with open(settings.ICS_LOCATION, "r") as ics_file:
         calendar = Calendar(ics_file.read())
 
-    future_events = list(
-        {
-            event
-            for event in calendar.events
-            if event.begin.date() >= datetime.date.today()
-        }
-    )
+    all_events = list({event for event in calendar.events})
+
+    if when == "future":
+        events = [
+            event for event in all_events if event.begin.date() >= datetime.date.today()
+        ]
+    elif when == "today":
+        events = [
+            event for event in all_events if event.begin.date() == datetime.date.today()
+        ]
+    else:
+        events = all_events
+
+    return events
+
+
+def quando(update, context):
+    future_events = get_events("future")
     next_event = min(future_events, key=lambda e: e.begin)
-    event = {
-        "title": next_event.name,
-        "date": next_event.begin.strftime("%d/%m/%Y"),
-        "url": next_event.url,
-    }
-    next_event_msg = f"Vai rolar \"{event['title']}\" em {event['date']}. Mais informações em {event['url']}."
-    context.bot.send_message(update.message.chat_id, text=next_event_msg)
+    if next_event:
+        event = {
+            "title": next_event.name,
+            "date": next_event.begin.strftime("%d/%m/%Y"),
+            "url": next_event.url,
+        }
+        next_event_msg = f"Vai rolar \"{event['title']}\" em {event['date']}. Mais informações em {event['url']}."
+        context.bot.send_message(update.message.chat_id, text=next_event_msg)
+    else:
+        context.bot.send_message(
+            update.message.chat_id,
+            text="Não existe nenhum evento agendado até o momento.",
+        )
 
 
 def generate_ics(context):
@@ -36,23 +55,14 @@ def generate_ics(context):
 
 
 def pin_today_event(context):
-    with open(settings.ICS_LOCATION, "r") as ics_file:
-        calendar = Calendar(ics_file.read())
-
-    today_events = list(
-        {
-            event
-            for event in calendar.events
-            if event.begin.date() == datetime.date.today()
-        }
-    )
+    today_events = get_events("today")
     today_event = min(today_events, key=lambda e: e.begin)
-    event = {
-        "title": today_event.name,
-        "date": today_event.begin.strftime("%d/%m/%Y"),
-        "url": today_event.url,
-    }
-    if event:
+    if today_event:
+        event = {
+            "title": today_event.name,
+            "date": today_event.begin.strftime("%d/%m/%Y"),
+            "url": today_event.url,
+        }
         today_event_msg = f"**Hoje** {event['date']} vai rolar \"{event['title']}\". Mais informações em {event['url']}."
 
         message = context.bot.send_message(
@@ -60,5 +70,12 @@ def pin_today_event(context):
         )
 
         context.bot.pin_chat_message(
-            message.chat_id, message.message_id, disable_notification=False
+            settings.LHC_CHAT_ID, message.message_id, disable_notification=False
         )
+    else:
+        chat = update.message.bot.get_chat(chat_id)
+        pinned_message = chat.pinned_message
+        if pinned_message.from_user.username == "lhc_joker_bot":
+            context.bot.pin_chat_message(
+                settings.LHC_CHAT_ID, pinned_message.message_id
+            )
